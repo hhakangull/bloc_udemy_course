08b22e96fbc371d65ed1d548278262481c78b113
Hakan GÜL <hakanngul@icloud.com>
2022-10-30 20:12:00 +0300

    internetCubit final

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,46 @@
+import 'package:bloc_udemy_course/logic/cubit/counter_cubit.dart';
+import 'package:bloc_udemy_course/logic/cubit/internet_cubit.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-import 'logic/cubit/counter_cubit.dart';
 import 'presentation/router/app_router.dart';
 
 void main() {
-  final CounterState counterState1 = CounterState(counterValue: 1);
-  final CounterState counterState2 = CounterState(counterValue: 1);
-  //! THIS SHOULD RETURN TRUE NOW
-  //print(counterState1 == counterState2);
-  runApp(const MyApp());
+  Connectivity connectivity = Connectivity();
+  InternetCubit internetCubit = InternetCubit(connectivity: connectivity);
+  runApp(MyApp(
+    appRouter: AppRouter(internetCubit: internetCubit),
+    internetCubit: InternetCubit(connectivity: connectivity),
+    connectivity: connectivity,
+  ));
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  final AppRouter _appRouter = AppRouter();
+class MyApp extends StatelessWidget {
+  final AppRouter appRouter;
+  final Connectivity connectivity;
+  final InternetCubit internetCubit;
+  const MyApp({
+    super.key,
+    required this.appRouter,
+    required this.connectivity,
+    required this.internetCubit,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<InternetCubit>(create: (context) => InternetCubit(connectivity: connectivity)),
+        BlocProvider<CounterCubit>(create: (context) => CounterCubit(internetCubit: context.read<InternetCubit>()))
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        onGenerateRoute: appRouter.onGenerateRoute,
       ),
-      onGenerateRoute: _appRouter.onGenerateRoute,
     );
-  }
-
-  @override
-  void dispose() {
-    _appRouter.dispose();
-    super.dispose();
   }
 }
